d6f798dc054eb5de8911c03aecfe490f3ca636fb
Zane Stock <zanestock3@gmail.com>
2022-05-25 06:20:22 -0500

    I have forgotten to commit, have been working on project over past 9 days and have most fundamentals complete

---
+++ NOTES.md
@@ -0,0 +1,23 @@
+### Random Meme Generator
+
+An open auth API which has a list of memes which I can pull to access them all, click function to see a different meme every time with the ability to delete a meme if you don't lile it t all. Maybe be able to comment on joke as well.
+
+### Features
+* Random Meme API
+* Show Random Meme (DOMContentLoaded)
+* Like meme (click)
+* Delete meme (click)
+* Comment on meme (submit) (need a json server)
+
+### MVP
+* Going to need to load the jokes
+* Show Random Meme (DOMContentLoaded)
+* Delete meme (click)
+* Comment on meme (submit) (need a json server)
+
+
+### Stretch
+* Like meme (click)
+
+### CSS
+* https://materializecss.com/
---
+++ index.js
@@ -0,0 +1,132 @@
+// defined variables
+const primaryDiv = () => document.getElementById("primary");
+const randomGeneratorLink = () => document.getElementById("random-generator");
+const homePage = () => document.getElementById("home");
+const beers = () => document.getElementById("all-beers")
+let likedBeers = [];
+
+// event listeners
+function randomBeerClickEvent() {
+    randomGeneratorLink().addEventListener("click", fetchRandomBeer);
+}
+
+function returnToHome() {
+    homePage().addEventListener("click", renderHomePage);
+}
+
+function allBeersPage() {
+    beers().addEventListener("click", renderAllBeers);
+}
+
+// Event Handlers
+function renderHomePage() {
+    // create the html elements for the home page
+    resetPrimaryDiv();
+
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+    const img = document.createElement("img");
+
+    h3.innerText = "Random Beer Generator"
+    p.innerText = "Are you a beer enthusiast looking at expanding your pallet? Join us as we on embark on our random beer generator journey, where this application can provide you with some of the most unique beers you have never heard of! If a particular beer sounds tasty to you, let us know and we will save it to your favorites page!"
+    img.src = "https://images.punkapi.com/v2/192.png"
+
+    primaryDiv().appendChild(h3);
+    primaryDiv().appendChild(p);
+    primaryDiv().appendChild(img);
+}
+
+function renderBeerPage(beer) {
+    resetPrimaryDiv();
+
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+    const btn = document.createElement("button")
+    
+    h3.innerText = "Random Beer"
+    p.innerText = beer;
+    btn.innerText = "Sounds Tasty!"
+    btn.classList.add("btn")
+
+    btn.addEventListener("click", () => tastyBeer(beer)); 
+  
+    primaryDiv().appendChild(h3);
+    primaryDiv().appendChild(p);
+    primaryDiv().appendChild(btn);
+}
+
+function renderAllBeers() {
+    resetPrimaryDiv();
+
+    const h3 = document.createElement("h3");
+    const h5 = document.createElement("h5"); 
+
+    h3.innerText = "Favorite Beers"
+    h5.innerText = "Here is a list of all your favorite Beers!"
+
+   
+    primaryDiv().appendChild(h3);
+    primaryDiv().appendChild(h5);
+    
+    renderBeer()
+}
+
+// Render and iterate over the liked beers
+const renderBeer = () => {
+    const ul = document.createElement("ul");
+    likedBeers.forEach(beer => newRenderBeer(beer, ul))
+    primaryDiv().appendChild(ul);
+}
+
+const newRenderBeer = (beer, ul) => {
+    const li = document.createElement("li");
+    li.innerText = beer.beer
+    ul.appendChild(li);
+}
+
+// Beer API Calls
+const fetchRandomBeer = () => {
+    fetch("https://api.punkapi.com/v2/beers/random")
+        .then(resp => resp.json())
+        .then(data => {
+           renderBeerPage(data[0].name)
+        })
+}
+
+const fetchLikedBeer = () => {
+    fetch("http://localhost:3000/liked")
+    .then(resp => resp.json())
+    .then(data => likedBeers = data);
+}
+
+const tastyBeer = beer => {
+    fetch ("http://localhost:3000/liked", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({beer: beer})
+    })
+    .then(resp =>resp.json())
+    .then(data => {
+        likedBeers.push(data);
+        renderAllBeers();
+
+    })
+}
+
+// Extra
+function resetPrimaryDiv() {
+    primaryDiv().innerHTML = ""
+}
+
+// DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
+    renderHomePage();
+    fetchLikedBeer();
+    randomBeerClickEvent();
+    returnToHome();
+    allBeersPage();
+    
+})
